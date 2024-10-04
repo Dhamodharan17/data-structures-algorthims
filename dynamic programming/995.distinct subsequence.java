1e380c6176993c53275ab2238f124845bcb7d850
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-10-04 14:35:03 +0530

    Create 995.distinct subsequence.java

---
+++ dynamic programming/995.distinct subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int n1 = s.length();
+        int n2 = t.length();
+        return findLCS(n1-1,n2-1,s,t);
+
+    }
+
+    int findLCS(int s1,int t1,String s,String t){
+
+        if(t1<0) return 1;//1 common subseq ""
+
+        if(s1<0) return 0;
+
+        if(s.charAt(s1) == t.charAt(t1)){
+          //Input: s = "babgbag", t = "bag"
+          //if we both we will miss to match bag since will lose g.
+          //also there might be multiple occurence of t1 in s2
+            return findLCS(s1-1,t1-1,s,t) + findLCS(s1-1,t1,s,t);
+        }else{
+            return findLCS(s1-1,t1,s,t);
+        }
+
+    }
+
+}
