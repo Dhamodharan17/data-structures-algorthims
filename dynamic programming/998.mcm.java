bcf293bb808f16d63c04bc38d4a1ad3e38500569
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-10-04 18:25:29 +0530

    Update 998.mcm.java

--- dynamic programming/998.mcm.java
+++ dynamic programming/998.mcm.java
@@ -24,3 +24,31 @@ class Solution{
     }
     
 }
+//tabulation (oppsite) start from small build to big
+
+
+class Solution{
+    static int matrixMultiplication(int N, int arr[])
+    {
+        int dp[][] = new int[N][N];
+        for(int i=0;i<N;i++)dp[i][i]=0;
+      
+      //till 1 because array start at 1
+      for(int i=N-1;i>=1;i--){
+          //block
+          for(int j=i+1;j<N;j++){
+              int mini = Integer.MAX_VALUE;
+              //parition
+              for(int k=i;k<j;k++){
+                  
+                  int steps = arr[i-1]*arr[k]*arr[j] + dp[i][k] + dp[k+1][j];
+                  mini = Math.min(mini,steps);
+              }
+              dp[i][j] = mini;
+          }
+      }
+      
+     return dp[1][N-1];
+        
+    }
+
