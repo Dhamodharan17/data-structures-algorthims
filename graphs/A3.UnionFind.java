52d904284c4c5f2d7209f33ff2b05d41ab3807b9
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-10-05 12:14:27 +0530

    Update A3.UnionFind.java

--- graphs/A3.UnionFind.java
+++ graphs/A3.UnionFind.java
@@ -49,3 +49,62 @@ class Solution {
         return dsu.comp;
     }
 }
+
+
+//tabulation
+
+class Solution {
+    
+    static class DSU{
+        int parent[];
+        int rank[];
+        int comp;
+        
+        DSU(int n){
+            parent = new int[n];
+            rank = new int[n];
+            comp=n;
+            for(int i=0;i<n;i++)
+                parent[i] = i;
+        }
+        
+        int find(int x){
+            if(x == parent[x]) return x;
+            
+            return find(parent[x]);
+        }
+        
+        void union(int x,int y){
+            
+            int px = find(x),py= find(y);
+            
+            if(py==px) return;
+            comp--;
+            
+            if(rank[px]>rank[py]){
+                parent[py] = px;
+            }else if(rank[py] > rank[px]){
+                parent[px] = py;
+            }else{
+                parent[py] = px;
+                rank[px]++;
+            }
+        }
+    }
+    
+    static int numProvinces(ArrayList<ArrayList<Integer>> adj, int V) {
+    
+        DSU dsu = new DSU(V+1);
+    
+        for(int i=0;i<V;i++){
+            for(int next:adj.get(i)){
+                if(next==0 || dsu.find(i) == dsu.find(next)) continue;
+                dsu.union(i,next);
+            }
+        }
+        
+        return dsu.comp;
+    }
+
+    
+};
