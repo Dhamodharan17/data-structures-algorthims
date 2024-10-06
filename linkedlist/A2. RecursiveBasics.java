01873ffb4a80012391ec565cdb0670fbec7cb3df
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-10-06 19:01:31 +0530

    Create A2. RecursiveBasics.java

---
+++ linkedlist/A2. RecursiveBasics.java
@@ -0,0 +1,63 @@
+//print
+class Solution {
+    void display(Node head) {
+
+        if(head == null) return;//base case
+        System.out.print(head.data+" ");
+        display(head.next);//recursion
+        
+    }
+}
+
+//insert
+class Solution {
+    Node insertAtEnd(Node head, int x) {
+        
+        // insert = attach
+        
+        // at end return newnode
+        if(head == null) return new Node(x);
+        
+        // if next is null we will get new node from r1 or same node from r2
+        head.next = insertAtEnd(head.next,x);
+        
+        return head;//return same node
+     
+    }
+}
+
+//insert at pos
+    public static SinglyLinkedListNode insertNodeAtPosition(SinglyLinkedListNode head,
+     int data, int position) {
+ 
+     if (position == 0) {
+        SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+        newNode.next = head;
+        // return the newnode with move the exisiting to after it
+        return newNode;
+    }
+    
+    // Recursive case: move to the next node and decrease the position
+    head.next = insertNodeAtPosition(head.next, data, position - 1);
+    return head;//return the same node
+
+    }
+
+}
+
+//delete
+
+class GfG
+{
+//delete = skip
+Node deleteNode(Node head, int x){
+	
+	//when given position skip and return the next
+	if(x==1) return head.next;
+	
+	head.next = deleteNode(head.next,--x);
+	
+	return head;//return the same node came in parameter
+		
+    }
+}
