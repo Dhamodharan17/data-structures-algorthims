db7b2e7142b09af4d5819ea46805a54833d1dc4b
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-10-19 00:17:39 +0530

    Update subarraysumk.java

--- array/prefixsum/subarraysumk.java
+++ array/prefixsum/subarraysumk.java
@@ -1,3 +1,26 @@
+//prefix sum
+class Solution {
+    public int subarraySum(int[] arr, int k) {
+        int count = 0, n = arr.length;
+        int arraySum = 0;
+        Map<Integer, Integer> countMap = new HashMap<>();
+        countMap.put(0, 1);
+
+        for (int i = 0; i < n; i++) {
+            arraySum = arraySum + arr[i];
+            // does any subarray i can ignore
+            int prefix = arraySum - k;
+
+            if (countMap.containsKey(prefix)) {
+                // number of subarray having prefix
+                count = count + countMap.get(prefix);
+            }
+            countMap.put(arraySum, countMap.getOrDefault(arraySum, 0) + 1);
+        }
+        return count;
+    }
+}
+
 //brute force
 class Solution {
     public int subarraySum(int[] nums, int k) {
