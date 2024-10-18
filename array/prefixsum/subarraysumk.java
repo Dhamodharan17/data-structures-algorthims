886a7c78889104929b180bdf3dae387fa2f5d639
Dhamodharan Sundararjan <30789057+Dhamodharan17@users.noreply.github.com>
2024-10-19 01:12:56 +0530

    Update subarraysumk.java

--- array/prefixsum/subarraysumk.java
+++ array/prefixsum/subarraysumk.java
@@ -1,3 +1,19 @@
+//brute force
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count=0, n = nums.length;
+        for(int i=0;i<n;i++){
+            int sum = nums[i];
+            if(sum==k) count++;
+            for(int j=i+1;j<n;j++){
+                sum += nums[j];
+                if(sum == k) count++;
+            }
+        }  
+        return count;
+    }
+}
+
 //prefix sum
 class Solution {
     public int subarraySum(int[] arr, int k) {
@@ -20,20 +36,3 @@ class Solution {
         return count;
     }
 }
-
-//brute force
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int count=0, n = nums.length;
-        for(int i=0;i<n;i++){
-            int sum = nums[i];
-            if(sum==k) count++;
-            for(int j=i+1;j<n;j++){
-                sum += nums[j];
-                if(sum == k) count++;
-            }
-        }  
-        return count;
-    }
-}
-
